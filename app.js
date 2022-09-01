cfe9e365a029b025cbff9b341bd5dfe2238392ce
Ezequiel Insua <ezequiel.insua@claro.com.ar>
2022-09-01 19:42:31 -0300

    V1.0 - server_express

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const Container = require('./container.js');
+
+const PORT = 8080;
+const fileName = 'productos.txt';
+const app = express();
+
+const contenedor = new Container(fileName);
+
+
+app.get('/productos', async (req, res) => {
+    try {
+        const products = await contenedor.getAll();
+        res.send( JSON.stringify( products ) );
+    } catch {
+        res.send("Lo sentimos. Ha ocurrido un error. Intente nuevamente mas tarde.")
+    }
+    
+})
+
+app.get('/productoRandom', async (req, res) => {
+    try {
+        const products = await contenedor.getAll();
+        const index = Math.round(Math.random() * products.length);
+        res.send( JSON.stringify( products[index] ) ) ;
+    } catch {
+        res.send("Lo sentimos. Ha ocurrido un error. Intente nuevamente mas tarde.")
+    }
+})
+
+
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+});
+
+server.on( "Error", error => console.log(`Error while listening on port ${PORT}: ${error}`) );
